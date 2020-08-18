3e18ef350ef59b0c15276c223e4ce96df89ea763
MaulanaAdil <kenari222@gmail.com>
2020-08-18 13:26:16 +0700

    first commit

---
+++ app/src/main/java/com/maulnad/mynotesapp/CustomOnclickListener.kt
@@ -0,0 +1,17 @@
+package com.maulnad.mynotesapp
+
+import android.view.View
+
+class CustomOnclickListener(
+    private val position: Int,
+    private val onItemCallback: OnItemClickCallback
+) : View.OnClickListener {
+
+    override fun onClick(v: View) {
+        onItemCallback.onItemClicked(v, position)
+    }
+    interface OnItemClickCallback {
+        fun onItemClicked(view: View, position: Int)
+    }
+}
+
